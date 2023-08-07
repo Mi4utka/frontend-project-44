648025466331d49230179b021f3f8000184e7751
Mi4utka <122639288+Mi4utka@users.noreply.github.com>
2023-08-07 22:10:43 +0300

    Update isPrimeNumber.js

--- functions/isPrimeNumber.js
+++ functions/isPrimeNumber.js
@@ -1,7 +1,8 @@
-const isPrimeNumer = (numb) => {
+const isPrimeNumber = (numb) => {
   let arr  = []
   for (let i = 0; i <= numb; i += 1) {
     if ( numb % i === 0) { 
       arr.push(i)}
   }
-    return arr.length === 2
+    return arr.length === 2 
+}
