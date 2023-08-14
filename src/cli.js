03edd532f88a0f8ac29c5c66e155182ec4165511
Mi4utka <mix.maisaa@mail.ru>
2023-08-14 16:35:50 +0300

    minor fixxxxx

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 
 function func() {
   let userName  = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
-    console.log('Hello ' + userName + '!');
+   console.log('Hello, ' + userName + '!');
+  
 }
 export default func
